5218b37416b9930a78dc85d7e255df7552236be6
Shoaib Qureshi <Shoaib.Qureshi@genmills.com>
2022-03-04 19:29:51 +0530

    Removing not needed files

--- SundayTechTest/Controllers/HomeController.cs
+++ SundayTechTest/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SundayTechTest.DAL;
-using SundayTechTest.ViewModels;
 
 
 namespace SundayTechTest.Controllers
@@ -17,34 +16,6 @@ namespace SundayTechTest.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            // Commenting out LINQ to show how to do the same thing in SQL.
-            //IQueryable<EnrollmentDateGroup> = from student in db.Students
-            //           group student by student.EnrollmentDate into dateGroup
-            //           select new EnrollmentDateGroup()
-            //           {
-            //               EnrollmentDate = dateGroup.Key,
-            //               StudentCount = dateGroup.Count()
-            //           };
-
-            // SQL version of the above LINQ code.
-            string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
-                + "FROM Person "
-                + "WHERE Discriminator = 'Student' "
-                + "GROUP BY EnrollmentDate";
-            IEnumerable<EnrollmentDateGroup> data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
-
-            return View(data.ToList());
-        }
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
